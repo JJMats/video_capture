05bde777f39801ab744b25abf039c99a7cd0c526
JJMats <matsonjjl@gmail.com>
2019-09-09 16:23:47 -0700

    feat: Create video recorder - Changed original video_capture.py to image_capture.py - Created new video_capture.py to record video from camera

--- video_capture.py
+++ video_capture.py
@@ -1,31 +1,71 @@
 import numpy as np
-import cv2
+import argparse, cv2, os
+
+def check_for_existing_file(fn):
+    video_path = 'videos/' + fn
+    if os.path.exists(video_path) and os.path.isfile(video_path):
+        return True
+
+    return False
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Process flags')
+parser.add_argument('-o',
+                    dest='outfile', 
+                    type=str,
+                    default='out',
+                    help='Output filename')
+parser.add_argument('r',
+                    dest='rate',
+                    type=str,
+                    default=5,
+                    help='Frame record rate - frames per second')
+args = parser.parse_args()
+
+
+# Check for existing file and append a number to the end if it exists
+output_fn = args.outfile
+video_index = 1
+while(check_for_existing_file(output_fn)):
+    # TODO: Strip the file extension if necessary
+    output_fn = output_fn + str(video_index)
+
+# Add file extension
+output_fn += '.avi'
 
 cap = cv2.VideoCapture(0)
-img_index = 0
 
-while(True):
+# Define the codec and create the VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(output_fn, fourcc, 20.0, (1280, 720))
+
+while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Perform operations on the frame
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret == True:
 
-    # Display the modified frame
-    #cv2.imshow('frame', gray)
-    cv2.imshow('frame', frame)
+        # Perform operations on the frame
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # frame = cv2.flip(frame, 0)
 
-    # Get keypress index
-    key = cv2.waitKey(1)
+        # Write the frame
+        out.write(frame)
+        
+        # Display the modified frame
+        #cv2.imshow('frame', gray)
+        cv2.imshow('frame', frame)
 
-    if key == 27 or key == ord('q'):
-        # Exit on Escape or "q" key press
-        break
-    elif key == ord('s'):
-        cv2.imwrite('pictures/image' + str(img_index) + '.jpeg', frame)
-        img_index += 1
-        print('Created image:', img_index)
+        # Get keypress index
+        key = cv2.waitKey(1)
+
+        if key == 27 or key == ord('q'):
+            # Exit on Escape or "q" key press
+            break
+
 
 # Release the resources
 cap.release()
+out.release()
 cv2.destroyAllWindows()
