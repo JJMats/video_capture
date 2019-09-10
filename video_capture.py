e211aff0f8700f5b32711397513ae069e87c5e87
Jeremy <matsonjjl@gmail.com>
2019-09-09 23:14:10 -0700

    feat: Add SIGINT shutdown method

--- video_capture.py
+++ video_capture.py
@@ -1,5 +1,9 @@
 import numpy as np
 import argparse, cv2, os
+import signal, sys
+
+#def signal_handler(sig, frame):
+#  sys.exit(0)
 
 def check_for_existing_file(fn):
     video_path = 'videos/' + fn
@@ -12,7 +16,7 @@ def check_for_existing_file(fn):
 # Parse arguments
 parser = argparse.ArgumentParser(description="Process flags")
 parser.add_argument('-o', dest='outfile', type=str, default='out', help="Output filename")
-parser.add_argument('r', dest='rate', type=str, default=5, help="Frame record rate - frames per second")
+#parser.add_argument('r', dest='rate', type=str, default=5, help="Frame record rate - frames per second")
 args = parser.parse_args()
 
 
@@ -24,14 +28,14 @@ while(check_for_existing_file(output_fn)):
     output_fn = output_fn + str(video_index)
 
 # Add file extension
-output_fn += '.mp4'
+output_fn = 'videos/' + output_fn + '.mp4'
 
 cap = cv2.VideoCapture(0)
 
 
 # Define the codec and create the VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(output_fn, fourcc, 20.0, (1280, 720))
+fourcc = cv2.VideoWriter_fourcc(*'FFV1')
+out = cv2.VideoWriter(output_fn, fourcc, 20.0, (640, 480))
 
 # Record the video from the camera
 while(cap.isOpened()):
@@ -46,16 +50,20 @@ while(cap.isOpened()):
 
         # Write the frame
         out.write(frame)
-        
+
         # Display the modified frame
         #cv2.imshow('frame', gray)
-        cv2.imshow('frame', frame)
+        #cv2.imshow('frame', frame)
 
         # Get keypress index
         key = cv2.waitKey(1)
 
         if key == 27 or key == ord('q'):
             # Exit on Escape or "q" key press
+            break
+
+        if not signal.SIGINT:
+            print("Received SIGINT")
             break
 
 
